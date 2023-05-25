44f7f370a8de8d9782eedc6e7ca09fa42670a96e
김영욱 <duddnr787@naver.com>
2023-05-26 00:20:21 +0900

    - 로그인 수정 -

--- src/component/Banner.js
+++ src/component/Banner.js
@@ -1,16 +1,44 @@
-import React from 'react';
+import { fontSize, height } from '@mui/system';
+import React, { useState } from 'react';
+import { Button, Container, Form } from 'react-bootstrap';
 //popularMovies에 있는 1번째 애를 배너로 만들거다.
 const Banner = () => {
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // 이메일 등록 또는 재시작 처리
+    // ...
+
+    // 폼 제출 후 입력값 초기화
+    setEmail('');
+  };
   return (
-    <div className='banner' style={{backgroundImage:"url(" + 'https://www.themoviedb.org/t/p/w1066_and_h600_bestv2/tcYO8ay3A0liCWxHu2creU3Q9IB.jpg' + ")"}}>
-      <div className='banner-info'>
-        <h1>귀멸의 칼날: 상현집결, 그리고 도공 마을로 (2023)</h1><br/>
-        <span>혈귀가 숨어있는 거리에 잠입한 탄지로 일행. 강력한 상현 6 혈귀 남매 규타로 & 다키와의 전투 끝에 탄지로 일행은 궁지에 몰린다. 
-          <br />
-          절체절명 위기의 순간에도 흔들리지 않는 곧은 의지로 규타로에 맞서는 탄지로, 젠이츠, 이노스케 그리고 음주 우즈이 텐겐. 환락의 거리 속 혈귀를 쓰러트리기 위한 그들의 치열한 전투가 시작된다. 
-          <br />
-          한편, 키부츠지 무잔은 무한성에 상현 혈귀들을 소집시키고 탄지로는 새로운 칼을 찾아 도공 마을로 향하는데...
-        </span>
+    <div className='banner' style={{ backgroundImage: "url(" + 'https://www.themoviedb.org/t/p/w1066_and_h600_bestv2/tcYO8ay3A0liCWxHu2creU3Q9IB.jpg' + ")" }}>
+      <div className='banner-info' >
+        <h1>귀멸의 칼날: 상현집결, 그리고 도공 마을로 (2023)</h1><br />
+        <br />
+        절체절명 위기의 순간에도 흔들리지 않는 곧은 의지로 규타로에 맞서는 탄지로, 젠이츠, 이노스케 그리고 음주 우즈이 텐겐. 환락의 거리 속 혈귀를 쓰러트리기 위한 그들의 치열한 전투가 시작된다.
+        <br />
+        한편, 키부츠지 무잔은 무한성에 상현 혈귀들을 소집시키고 탄지로는 새로운 칼을 찾아 도공 마을로 향하는데...
+
+        <div style={{marginTop:'30px', fontSize:'30px'}}>시청할 준비가 되셨나요? 가입을 하기 위해 아래 이메일 주소를 입력해주세요.</div>
+        <Container className="banner-container" style={{marginTop:'30px'}}>
+          <Form onSubmit={handleSubmit} style={{display:'flex'}}>
+            <Form.Group controlId="email" style={{marginRight:'10px'}}>
+              <Form.Control style={{backgroundColor: 'rgba(60, 60, 60, 0.5)', color:'white', width:'400px', height:'50px'}}
+                type="email"
+                placeholder="이메일 주소를 입력하세요."
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </Form.Group>
+            <Button variant="danger" type="submit">
+              시작하기 &gt;
+            </Button>
+          </Form>
+        </Container>
       </div>
     </div>
   );
--- src/component/LoginForm.js
+++ src/component/LoginForm.js
@@ -1,7 +1,7 @@
-import { padding } from '@mui/system';
 import React, { useState } from 'react';
 import { Button, Container } from 'react-bootstrap';
 import Form from 'react-bootstrap/Form';
+import { Link } from 'react-router-dom';
 
 const LoginForm = () => {
   const [id, setId] = useState('');
@@ -13,41 +13,52 @@ const LoginForm = () => {
   };
 
   return (
-    <Container className="mt-5" style={{width:'40%', height:'70%'}}>
-      <Form onSubmit={handleSubmit} style={{padding:'30px'}}>
+    <Container className="mt-5" style={{ width: '40%', height: '70%' }}>
+      <Form onSubmit={handleSubmit} style={{ padding: '30px' }}>
         <Form.Group controlId="email">
-          <Form.Label style={{color:"white",fontSize:'32px',marginBottom:'30px' }}>로그인</Form.Label>
+          <Form.Label style={{ color: "white", fontSize: '32px', marginBottom: '30px' }}>로그인</Form.Label>
           <Form.Control
             type="email"
             placeholder="이메일을 입력하세요"
             value={id}
             onChange={(e) => setId(e.target.value)}
-            style={{ backgroundColor: 'rgb(60, 60, 60)', width: '100%', height:'50px', color:'lightgray', borderColor:'rgb(27, 27, 27)' }}
+            style={{ backgroundColor: 'rgb(60, 60, 60)', width: '100%', height: '50px', color: 'lightgray', borderColor: 'rgb(27, 27, 27)' }}
           />
         </Form.Group>
-        <br/>
+        <br />
         <Form.Group controlId="password">
           <Form.Control
             type="password"
             placeholder="비밀번호를 입력하세요"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            style={{ backgroundColor: 'rgb(60, 60, 60)', width: '100%', height:'50px', color:'lightgray', borderColor:'rgb(27, 27, 27)' }}
+            style={{ backgroundColor: 'rgb(60, 60, 60)', width: '100%', height: '50px', color: 'lightgray', borderColor: 'rgb(27, 27, 27)' }}
           />
         </Form.Group>
-        <br/><br/>
-        <Button variant="outline-danger" type="submit" style={{ width: '100%',marginBottom:'10px'}}>
+        <div style={{ color: 'white', textAlign: 'right', marginTop: '10px' }}>
+          <Link to="/findPassword" style={{ color: 'white' }}>
+            비밀번호를 까먹으셨나요?
+          </Link>
+        </div>
+        <br /><br />
+        <Button variant="outline-danger" type="submit" style={{ width: '100%', marginBottom: '10px' }}>
           로그인
         </Button>
-        <Form.Group className="mb-3">
+        <br/><br/>
+        <Form>
           <Form.Check
-            type="checkbox"
-            id="disabledFieldsetCheck"
+            type="switch"
+            id="custom-switch"
             label="로그인 정보 저장"
-            checked='true'
-            style={{color:'#eee'}}
+            style={{ color: '#eee', marginLeft : '65%'}}
           />
-        </Form.Group>
+        </Form>
+        <div style={{ color: 'rgb(120, 120, 120)', textAlign: 'center', marginTop: '20px' }}>
+          Wookflix 회원이 아닌가요?{' '}
+          <Link to="/" style={{ color: 'white' }}>
+            지금 가입하세요.
+          </Link>
+        </div>
       </Form>
     </Container>
   );
